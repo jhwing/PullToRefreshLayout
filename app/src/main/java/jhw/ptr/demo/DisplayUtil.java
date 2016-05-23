1100eb64ce2139e6a18787d0c3a88ffd4b618fed
jihongwen <jhwing@163.com>
2016-05-23 10:26:35 +0800

    文字变化加载动画

---
+++ app/src/main/java/jhw/ptr/demo/DisplayUtil.java
@@ -0,0 +1,96 @@
+package jhw.ptr.demo;
+
+import android.content.res.Resources;
+import android.os.Build;
+import android.view.View;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by jihongwen on 15/10/21.
+ */
+public class DisplayUtil {
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    /**
+     * 生成view id
+     *
+     * @return
+     */
+    public static int generateViewId() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            for (; ; ) {
+                final int result = sNextGeneratedId.get();
+                // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+                int newValue = result + 1;
+                if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+                if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                    return result;
+                }
+            }
+        } else {
+            return View.generateViewId();
+        }
+    }
+
+    /**
+     * 根据手机密度从 dp 的单位 转成为 px(像素)
+     *
+     * @param dpValue
+     * @return
+     */
+    public static int dip2px(float dpValue) {
+        return (int) (dpValue * Resources.getSystem().getDisplayMetrics().density + 0.5f);
+    }
+
+    /**
+     * 根据手机密度从 px 的单位 转成为 dp
+     *
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(float pxValue) {
+        return (int) (pxValue / Resources.getSystem().getDisplayMetrics().density + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param fontScale（DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int sp2px(float fontScale) {
+        return (int) (fontScale * Resources.getSystem().getDisplayMetrics().scaledDensity + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param fontScale（DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int px2sp(float pxValue) {
+        return (int) (pxValue / Resources.getSystem().getDisplayMetrics().scaledDensity + 0.5f);
+    }
+
+    /**
+     * 获取手机屏幕高度
+     *
+     * @return heightPixels 单位px
+     */
+    public static int getScreenHeightPixels() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * 获取手机屏幕宽度
+     *
+     * @return widthPixels 单位px
+     */
+    public static int getScreenWidthPixels() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+}
--- app/src/main/java/jhw/ptr/demo/MainActivity.java
+++ app/src/main/java/jhw/ptr/demo/MainActivity.java
@@ -1,5 +1,6 @@
 package jhw.ptr.demo;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -32,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
         refreshLayout = (PullToRefreshLayout) findViewById(R.id.ptrLayout);
         refreshLayout.setPullRefresh(false);
         refreshHandler = new RefreshHeaderView(this);
-        refreshLayout.setHeaderView(refreshHandler, false);
+        StoreHouseHeader header = new StoreHouseHeader(this);
+        header.initWithPointList(getPointList());
+        refreshLayout.setHeaderView(header, false);
         refreshLayout.setOnRefreshListener(new PullToRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -86,5 +89,83 @@ public class MainActivity extends AppCompatActivity {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.title);
         }
+    }
+
+
+    private ArrayList<float[]> getPointList() {
+        // this point is taken from https://github.com/cloay/CRefreshLayout
+        List<Point> startPoints = new ArrayList<Point>();
+        startPoints.add(new Point(240, 80));
+        startPoints.add(new Point(270, 80));
+        startPoints.add(new Point(265, 103));
+        startPoints.add(new Point(255, 65));
+        startPoints.add(new Point(275, 80));
+        startPoints.add(new Point(275, 80));
+        startPoints.add(new Point(302, 80));
+        startPoints.add(new Point(275, 107));
+
+        startPoints.add(new Point(320, 70));
+        startPoints.add(new Point(313, 80));
+        startPoints.add(new Point(330, 63));
+        startPoints.add(new Point(315, 87));
+        startPoints.add(new Point(330, 80));
+        startPoints.add(new Point(315, 100));
+        startPoints.add(new Point(330, 90));
+        startPoints.add(new Point(315, 110));
+        startPoints.add(new Point(345, 65));
+        startPoints.add(new Point(357, 67));
+        startPoints.add(new Point(363, 103));
+
+        startPoints.add(new Point(375, 80));
+        startPoints.add(new Point(375, 80));
+        startPoints.add(new Point(425, 80));
+        startPoints.add(new Point(380, 95));
+        startPoints.add(new Point(400, 63));
+
+        List<Point> endPoints = new ArrayList<Point>();
+        endPoints.add(new Point(270, 80));
+        endPoints.add(new Point(270, 110));
+        endPoints.add(new Point(270, 110));
+        endPoints.add(new Point(250, 110));
+        endPoints.add(new Point(275, 107));
+        endPoints.add(new Point(302, 80));
+        endPoints.add(new Point(302, 107));
+        endPoints.add(new Point(302, 107));
+
+        endPoints.add(new Point(340, 70));
+        endPoints.add(new Point(360, 80));
+        endPoints.add(new Point(330, 80));
+        endPoints.add(new Point(340, 87));
+        endPoints.add(new Point(315, 100));
+        endPoints.add(new Point(345, 98));
+        endPoints.add(new Point(330, 120));
+        endPoints.add(new Point(345, 108));
+        endPoints.add(new Point(360, 120));
+        endPoints.add(new Point(363, 75));
+        endPoints.add(new Point(345, 117));
+
+        endPoints.add(new Point(380, 95));
+        endPoints.add(new Point(425, 80));
+        endPoints.add(new Point(420, 95));
+        endPoints.add(new Point(420, 95));
+        endPoints.add(new Point(400, 120));
+        ArrayList<float[]> list = new ArrayList<float[]>();
+
+        int offsetX = Integer.MAX_VALUE;
+        int offsetY = Integer.MAX_VALUE;
+
+        for (int i = 0; i < startPoints.size(); i++) {
+            offsetX = Math.min(startPoints.get(i).x, offsetX);
+            offsetY = Math.min(startPoints.get(i).y, offsetY);
+        }
+        for (int i = 0; i < endPoints.size(); i++) {
+            float[] point = new float[4];
+            point[0] = startPoints.get(i).x - offsetX;
+            point[1] = startPoints.get(i).y - offsetY;
+            point[2] = endPoints.get(i).x - offsetX;
+            point[3] = endPoints.get(i).y - offsetY;
+            list.add(point);
+        }
+        return list;
     }
 }
